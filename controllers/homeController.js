f0db10d150a4821f293e5fb8ba630c2d1b399a9d
KaberaSamuel <kaberanshutis@gmail.com>
2025-01-26 13:56:35 +0200

    putting featured items on the home page

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,7 +1,21 @@
 import { body } from "express-validator";
+import { getTableElement } from "../db/queries.js";
 
-function handleHomeReqs(req, res) {
-  res.render("index");
+async function getFeaturedCategories() {
+  const [action] = await getTableElement("categories", "action");
+  const [adventure] = await getTableElement("categories", "adventure");
+  const [racing] = await getTableElement("categories", "racing");
+  const [sports] = await getTableElement("categories", "sports");
+
+  return [action, adventure, racing, sports];
+}
+
+async function handleHomeReqs(req, res) {
+  const featuredCategories = await getFeaturedCategories();
+
+  res.render("index", {
+    featuredCategories: featuredCategories,
+  });
 }
 
 export { handleHomeReqs };
